bbaaa3c9c479454c59b1555018b5b9e17c0a276d
Zita Szupera <szuperaz@gmail.com>
2023-10-16 15:24:18 +0200

    blocklist API

---
+++ __tests__/block-lists.test.ts
@@ -0,0 +1,54 @@
+import "dotenv/config";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { BlockList, StreamClient } from "../";
+
+const apiKey = process.env.STREAM_API_KEY!;
+const secret = process.env.STREAM_SECRET!;
+
+describe("block lists CRUD API", () => {
+  let client: StreamClient;
+  let blockList: BlockList;
+
+  beforeEach(() => {
+    blockList = {
+        name: 'F1',
+        words: ['Ricciardo should retire']
+    }
+  });
+
+  beforeAll(() => {
+    client = new StreamClient(apiKey, secret);
+  });
+
+  it("create", async () => {
+    const response = await client.chat.createBlockList(blockList);
+
+    expect(response).toBeDefined();
+  });
+
+  it("list", async () => {
+    const listResponse = await client.chat.listBlockLists();
+
+    expect(listResponse.blocklists.find(b => b.name === blockList.name)).toBeDefined();
+
+    const getResponse = await client.chat.getBlockList({name: blockList.name});
+    
+    expect(getResponse.blocklist?.name).toBe(blockList.name);
+  });
+
+  it("update", async () => {
+    const response = await client.chat.updateBlockList(blockList.name, {words: [...blockList.words, 'R1cciardo should retire']});
+
+    expect(response).toBeDefined();
+
+    const updatedBlockList = await client.chat.getBlockList({name: blockList.name});
+
+    expect(updatedBlockList.blocklist?.words.length).toBe(2);
+  });
+
+  it("delete", async () => {
+    const response = await client.chat.deleteBlockList({name: blockList.name});
+
+    expect(response).toBeDefined();
+  });
+});
--- src/StreamChatClient.ts
+++ src/StreamChatClient.ts
@@ -1,10 +1,32 @@
 import { StreamClient } from "./StreamClient";
+import { CreateBlockListRequest, DeleteBlockListRequest, GetBlockListRequest, SettingsApi, UpdateBlockListRequest } from "./gen/chat";
 
 export class StreamChatClient {
+  private settingsApi: SettingsApi;
+
   constructor(private streamClient: StreamClient) {
-    const configuration = this.streamClient.getConfiguration({
-      basePath:
-        this.streamClient.basePath || "https://video.stream-io-api.com/video",
-    });
+    const configuration = this.streamClient.getConfiguration();
+    //@ts-expect-error typing problem
+    this.settingsApi = new SettingsApi(configuration);
+  }
+
+  createBlockList = (createBlockListRequest: CreateBlockListRequest) => {
+    return this.settingsApi.createBlockList({createBlockListRequest});
+  } 
+
+  listBlockLists = () => {
+    return this.settingsApi.listBlockLists();
+  }
+
+  getBlockList = (request: GetBlockListRequest) => {
+    return this.settingsApi.getBlockList(request);
+  }
+
+  updateBlockList = (name: string, updateBlockListRequest: UpdateBlockListRequest) => {
+    return this.settingsApi.updateBlockList({name, updateBlockListRequest});
+  }
+
+  deleteBlockList = (request: DeleteBlockListRequest) => {
+    return this.settingsApi.deleteBlockList(request);
   }
 }
