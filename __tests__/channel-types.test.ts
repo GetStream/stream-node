0125e7646b849f64d6e747bf66ee08a50082b963
Zita Szupera <szuperaz@gmail.com>
2023-10-17 09:05:49 +0200

    channel types endpoints

---
+++ __tests__/channel-types.test.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import { beforeAll, describe, expect, it } from "vitest";
+import { v4 as uuidv4 } from "uuid";
+import { CreateChannelTypeRequestAutomodEnum, StreamClient } from "../";
+
+
+const apiKey = process.env.STREAM_API_KEY!;
+const secret = process.env.STREAM_SECRET!;
+
+describe("channel types CRUD API", () => {
+  let client: StreamClient;
+  const channelType = uuidv4();
+
+  beforeAll(() => {
+    client = new StreamClient(apiKey, secret);
+  });
+
+  it("create", async () => {
+    const response = await client.chat.createChannelType({name: channelType, automod: CreateChannelTypeRequestAutomodEnum.DISABLED});
+
+    expect(response.name).toBe(channelType);
+  });
+
+  it("read", async () => {
+    const response = await client.chat.listChannelTypes();
+
+    expect(response.channel_types[channelType]).toBeDefined();
+  });
+
+  it("update", async () => {
+    const response = await client.chat.updateChannelType(channelType, {automod: CreateChannelTypeRequestAutomodEnum.SIMPLE});
+
+    expect(response.automod).toBe(CreateChannelTypeRequestAutomodEnum.SIMPLE);
+
+    const getResponse = await client.chat.getChannelType({name: channelType});
+
+    //@ts-expect-error typing problem
+    expect(getResponse.automod).toBe(CreateChannelTypeRequestAutomodEnum.SIMPLE);
+  });
+
+  it("delete", async () => {
+    const response = await client.chat.deleteChannelType({name: channelType});
+
+    expect(response).toBeDefined();
+  });
+});
--- src/StreamChatClient.ts
+++ src/StreamChatClient.ts
@@ -1,13 +1,16 @@
 import { StreamClient } from "./StreamClient";
-import { CreateBlockListRequest, DeleteBlockListRequest, GetBlockListRequest, SettingsApi, UpdateBlockListRequest } from "./gen/chat";
+import { ChannelTypesApi, CreateBlockListRequest, CreateChannelTypeRequest, DeleteBlockListRequest, DeleteChannelTypeRequest, GetBlockListRequest, GetChannelTypeRequest, SettingsApi, UpdateBlockListRequest, UpdateChannelTypeRequest } from "./gen/chat";
 
 export class StreamChatClient {
   private settingsApi: SettingsApi;
+  private channelTypesApi: ChannelTypesApi;
 
   constructor(private streamClient: StreamClient) {
     const configuration = this.streamClient.getConfiguration();
     //@ts-expect-error typing problem
     this.settingsApi = new SettingsApi(configuration);
+    //@ts-expect-error typing problem
+    this.channelTypesApi = new ChannelTypesApi(configuration);
   }
 
   createBlockList = (createBlockListRequest: CreateBlockListRequest) => {
@@ -28,5 +31,25 @@ export class StreamChatClient {
 
   deleteBlockList = (request: DeleteBlockListRequest) => {
     return this.settingsApi.deleteBlockList(request);
+  }
+
+  createChannelType = (createChannelTypeRequest: CreateChannelTypeRequest) => {
+    return this.channelTypesApi.createChannelType({createChannelTypeRequest});
+  }
+
+  deleteChannelType = (request: DeleteChannelTypeRequest) => {
+    return this.channelTypesApi.deleteChannelType(request);
+  }
+
+  getChannelType = (request: GetChannelTypeRequest) => {
+    return this.channelTypesApi.getChannelType(request);
+  }
+
+  listChannelTypes = () => {
+    return this.channelTypesApi.listChannelTypes();
+  }
+
+  updateChannelType = (name: string, updateChannelTypeRequest: UpdateChannelTypeRequest) => {
+    return this.channelTypesApi.updateChannelType({name, updateChannelTypeRequest});
   }
 }
