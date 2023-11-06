91b07b62305f11c8b8ed74fe3522d7eb9ec59632
Zita Szupera <szuperaz@gmail.com>
2023-11-06 16:47:27 +0100

    Update block list name

--- __tests__/block-lists.test.ts
+++ __tests__/block-lists.test.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { BlockList, StreamClient } from "../";
+import { v4 as uuidv4 } from "uuid";
 
 const apiKey = process.env.STREAM_API_KEY!;
 const secret = process.env.STREAM_SECRET!;
@@ -11,9 +12,9 @@ describe("block lists CRUD API", () => {
 
   beforeEach(() => {
     blockList = {
-        name: 'F1',
-        words: ['Ricciardo should retire']
-    }
+      name: "F1" + uuidv4(),
+      words: ["Ricciardo should retire"],
+    };
   });
 
   beforeAll(() => {
@@ -29,25 +30,35 @@ describe("block lists CRUD API", () => {
   it("list", async () => {
     const listResponse = await client.chat.listBlockLists();
 
-    expect(listResponse.blocklists.find(b => b.name === blockList.name)).toBeDefined();
+    expect(
+      listResponse.blocklists.find((b) => b.name === blockList.name)
+    ).toBeDefined();
 
-    const getResponse = await client.chat.getBlockList({name: blockList.name});
-    
+    const getResponse = await client.chat.getBlockList({
+      name: blockList.name,
+    });
+
     expect(getResponse.blocklist?.name).toBe(blockList.name);
   });
 
   it("update", async () => {
-    const response = await client.chat.updateBlockList(blockList.name, {words: [...blockList.words, 'R1cciardo should retire']});
+    const response = await client.chat.updateBlockList(blockList.name, {
+      words: [...blockList.words, "R1cciardo should retire"],
+    });
 
     expect(response).toBeDefined();
 
-    const updatedBlockList = await client.chat.getBlockList({name: blockList.name});
+    const updatedBlockList = await client.chat.getBlockList({
+      name: blockList.name,
+    });
 
     expect(updatedBlockList.blocklist?.words.length).toBe(2);
   });
 
   it("delete", async () => {
-    const response = await client.chat.deleteBlockList({name: blockList.name});
+    const response = await client.chat.deleteBlockList({
+      name: blockList.name,
+    });
 
     expect(response).toBeDefined();
   });
