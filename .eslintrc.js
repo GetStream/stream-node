542af13ca685d426a4b692919f74a616c2bdc904
Zita Szupera <szuperaz@gmail.com>
2023-11-23 09:18:32 +0100

    configure lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "standard-with-typescript",
+    "extends": ["standard-with-typescript", "prettier"],
     "overrides": [
         {
             "env": {
@@ -22,5 +22,11 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "semi": "off",
+        "@typescript-eslint/semi": "off",
+        "@typescript-eslint/promise-function-async": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/comma-dangle": "off",
+        "@typescript-eslint/consistent-type-imports": "off"
     }
 }
