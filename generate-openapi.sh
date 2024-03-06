7965326321577033c66e4858bb6986049dbcda37
Zita Szupera <szuperaz@gmail.com>
2024-03-06 14:44:49 +0100

    chore: update generator to allow updating chat or video only

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -2,76 +2,98 @@
 set -euo pipefail
 
 FROM_REPO=$1;
+DEFAULT_PRODUCT="all";
+PRODUCT="${2:-$DEFAULT_PRODUCT}";
 
-if  [ "$FROM_REPO" == 'chat' ]; then
-  PROTOCOL_REPO_DIR="../chat"
-else
-  PROTOCOL_REPO_DIR="../protocol"
-fi
-if  [ "$FROM_REPO" == 'chat' ]; then
-  SCHEMA_FILE="$PROTOCOL_REPO_DIR/releases/video-openapi.yaml"
-else
-  SCHEMA_FILE="$PROTOCOL_REPO_DIR/openapi/video-openapi.yaml"
+echo $PRODUCT;
+
+if [ "$PRODUCT" != 'chat' ] && [ "$PRODUCT" != 'video' ] && [ "$PRODUCT" != 'all' ]; then
+  echo "Invalid product option $PRODUCT"
+  exit 1
 fi
 
-if  [ "$FROM_REPO" == 'chat' ]; then
-  # Generate the Coordinator OpenAPI schema
-  make -C $PROTOCOL_REPO_DIR video-openapi
-fi
-
-OUTPUT_DIR="./src/gen"
 TEMP_OUTPUT_DIR="./openapi-temp"
 
-# Clean previous output
-rm -rf $TEMP_OUTPUT_DIR
-rm -rf $OUTPUT_DIR
+if [ "$PRODUCT" == 'video' ] || [ "$PRODUCT" == 'all' ] ; then
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    PROTOCOL_REPO_DIR="../chat"
+  else
+    PROTOCOL_REPO_DIR="../protocol"
+  fi
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    SCHEMA_FILE="$PROTOCOL_REPO_DIR/releases/video-openapi.yaml"
+  else
+    SCHEMA_FILE="$PROTOCOL_REPO_DIR/openapi/video-openapi.yaml"
+  fi
 
-mkdir $OUTPUT_DIR
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    # Generate the Coordinator OpenAPI schema
+    make -C $PROTOCOL_REPO_DIR video-openapi
+  fi
 
-# NOTE: https://openapi-generator.tech/docs/generators/typescript-fetch/
-# Generate the Coordinator API models
-yarn openapi-generator-cli generate \
-  -i "$SCHEMA_FILE" \
-  -g typescript-fetch \
-  -o "$TEMP_OUTPUT_DIR" \
-  --additional-properties=supportsES6=true \
-  --additional-properties=modelPropertyNaming=original \
-  --additional-properties=enumPropertyNaming=UPPERCASE \
-  --additional-properties=withoutRuntimeChecks=true \
-  --model-name-prefix=Video
+  OUTPUT_DIR="./src/gen/video"
 
-# Remove the generated API client, just keep the models
-cp -r $TEMP_OUTPUT_DIR $OUTPUT_DIR/video
-rm -rf $TEMP_OUTPUT_DIR
+  # Clean previous output
+  rm -rf $TEMP_OUTPUT_DIR
+  rm -rf $OUTPUT_DIR
 
+  mkdir $OUTPUT_DIR
 
-if  [ "$FROM_REPO" == 'chat' ]; then
-  PROTOCOL_REPO_DIR="../chat"
-else
-  PROTOCOL_REPO_DIR="../protocol"
+  # NOTE: https://openapi-generator.tech/docs/generators/typescript-fetch/
+  # Generate the Coordinator API models
+  yarn openapi-generator-cli generate \
+    -i "$SCHEMA_FILE" \
+    -g typescript-fetch \
+    -o "$TEMP_OUTPUT_DIR" \
+    --additional-properties=supportsES6=true \
+    --additional-properties=modelPropertyNaming=original \
+    --additional-properties=enumPropertyNaming=UPPERCASE \
+    --additional-properties=withoutRuntimeChecks=true \
+    --model-name-prefix=Video
+
+  # Remove the generated API client, just keep the models
+  cp -r $TEMP_OUTPUT_DIR/ $OUTPUT_DIR
+  rm -rf $TEMP_OUTPUT_DIR
 fi
-if  [ "$FROM_REPO" == 'chat' ]; then
-  SCHEMA_FILE="$PROTOCOL_REPO_DIR/releases/chat-openapi.yaml"
-else
-  SCHEMA_FILE="$PROTOCOL_REPO_DIR/openapi/chat-openapi.yaml"
+
+
+if [ "$PRODUCT" == 'chat' ] || [ "$PRODUCT" == 'all' ]; then
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    PROTOCOL_REPO_DIR="../chat"
+  else
+    PROTOCOL_REPO_DIR="../protocol"
+  fi
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    SCHEMA_FILE="$PROTOCOL_REPO_DIR/releases/chat-openapi.yaml"
+  else
+    SCHEMA_FILE="$PROTOCOL_REPO_DIR/openapi/chat-openapi.yaml"
+  fi
+
+  if  [ "$FROM_REPO" == 'chat' ]; then
+    # Generate the Coordinator OpenAPI schema
+    make -C $PROTOCOL_REPO_DIR chat-openapi
+  fi
+
+  OUTPUT_DIR="./src/gen/chat"
+
+  # Clean previous output
+  rm -rf $TEMP_OUTPUT_DIR
+  rm -rf $OUTPUT_DIR
+
+  mkdir $OUTPUT_DIR
+
+  # NOTE: https://openapi-generator.tech/docs/generators/typescript-fetch/
+  # Generate the Coordinator API models
+  yarn openapi-generator-cli generate \
+    -i "$SCHEMA_FILE" \
+    -g typescript-fetch \
+    -o "$TEMP_OUTPUT_DIR" \
+    --additional-properties=supportsES6=true \
+    --additional-properties=modelPropertyNaming=original \
+    --additional-properties=enumPropertyNaming=UPPERCASE \
+    --additional-properties=withoutRuntimeChecks=true
+
+  # Remove the generated API client, just keep the models
+  cp -r $TEMP_OUTPUT_DIR/ $OUTPUT_DIR
+  rm -rf $TEMP_OUTPUT_DIR
 fi
-
-if  [ "$FROM_REPO" == 'chat' ]; then
-  # Generate the Coordinator OpenAPI schema
-  make -C $PROTOCOL_REPO_DIR chat-openapi
-fi
-
-# NOTE: https://openapi-generator.tech/docs/generators/typescript-fetch/
-# Generate the Coordinator API models
-yarn openapi-generator-cli generate \
-  -i "$SCHEMA_FILE" \
-  -g typescript-fetch \
-  -o "$TEMP_OUTPUT_DIR" \
-  --additional-properties=supportsES6=true \
-  --additional-properties=modelPropertyNaming=original \
-  --additional-properties=enumPropertyNaming=UPPERCASE \
-  --additional-properties=withoutRuntimeChecks=true
-
-# Remove the generated API client, just keep the models
-cp -r $TEMP_OUTPUT_DIR $OUTPUT_DIR/chat
-rm -rf $TEMP_OUTPUT_DIR
