efaf71d01da5a3f546d3b06d46bf5804d9202fe1
Zita Szupera <szuperaz@gmail.com>
2025-05-29 12:26:07 +0200

    Add fid to Feed

--- src/StreamFeed.ts
+++ src/StreamFeed.ts
@@ -1,3 +1,7 @@
 import { FeedApi } from './gen-feeds/feeds/FeedApi';
 
-export class StreamFeed extends FeedApi {}
+export class StreamFeed extends FeedApi {
+  get fid() {
+    return `${this.group}:${this.id}`;
+  }
+}
