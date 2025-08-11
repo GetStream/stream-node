e9547cb228575cf361460e03f807b0c212d40f86
Zoran Miladinoski <zoki_m@live.com>
2025-08-11 16:45:58 +0200

    feat: [FEEDS 640] get or create feed group req expand (#131)

--- src/gen/feeds/FeedsApi.ts
+++ src/gen/feeds/FeedsApi.ts
@@ -1426,8 +1426,12 @@ export class FeedsApi {
     };
     const body = {
       default_visibility: request?.default_visibility,
+      activity_processors: request?.activity_processors,
+      activity_selectors: request?.activity_selectors,
+      aggregation: request?.aggregation,
       custom: request?.custom,
       notification: request?.notification,
+      ranking: request?.ranking,
     };
 
     const response = await this.apiClient.sendRequest<
