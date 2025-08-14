cf2c8c7dd7f1d160abb06947f46d33af94832752
Zita Szupera <szuperaz@gmail.com>
2025-08-14 11:19:51 +0200

    Use serverside api for API generation

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -6,6 +6,6 @@ CHAT_DIR="../chat"
 
 rm -rf $OUTPUT_DIR
 
-( cd $CHAT_DIR ; make openapi ; go run ./cmd/chat-manager openapi generate-client --language ts --spec ./releases/v2/feeds-serverside-api.yaml --output $OUTPUT_DIR )
+( cd $CHAT_DIR ; make openapi ; go run ./cmd/chat-manager openapi generate-client --language ts --spec ./releases/v2/serverside-api.yaml --output $OUTPUT_DIR )
 
 yarn lint:gen
