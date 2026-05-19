d6fd25a562f2f8ae459f61d50e4e701a44791108
Jana Naumoska <jnaumoskaa@gmail.com>
2026-05-19 04:29:11 +0200

    Remove comment

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -6,10 +6,6 @@ CHAT_DIR="../chat"
 
 rm -rf $OUTPUT_DIR
 
-# `make openapi` builds the chat-manager binary into ./build/ and regenerates
-# the spec files under ./releases/v2/. Reuse the built binary instead of
-# `go run`, which broke when chat-manager moved to projects/chat-manager/
-# (its own module under the monorepo restructure).
 ( cd $CHAT_DIR ; make openapi ; ./build/chat-manager openapi generate-client --language ts --spec ./releases/v2/serverside-api.yaml --output $OUTPUT_DIR )
 
 yarn lint:gen
